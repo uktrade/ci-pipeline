e8dfb5d625e009d71fe2b9375de7f9c768967bed
Bowen Sun <bowensun@gmail.com>
2017-08-04 00:29:04 +0100

    * minor improvement script function return

--- bootstrap.rb
+++ bootstrap.rb
@@ -46,7 +46,7 @@ end
 
 def save_env(file, data)
   File.open(file, 'w') { |file| file.truncate(0) }
-  data.each { |key, value|
+  return data.each { |key, value|
     File.open(file, 'a') { |file|
       file.puts "#{key}=#{value}" unless key.empty? || value.empty?
     }
