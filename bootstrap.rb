49d9e669df251f51b0898d10d3ebde9df622d62a
Bowen Sun <bowensun@gmail.com>
2017-08-03 19:37:57 +0100

    * improve logics, auto cleanup consul for removed environments, improve env file writing

--- bootstrap.rb
+++ bootstrap.rb
@@ -20,80 +20,45 @@ def validate(schema, data)
 end
 
 def consul_add(path, data)
-  RestClient.put("#{CONSUL}/#{path}", data)
+  return RestClient.put("#{CONSUL}/#{path}", data)
+end
+
+def consul_delete(path)
+  return RestClient.delete("#{CONSUL}/#{path}")
 end
 
 def consul_get(path)
-  return RestClient.get("#{CONSUL}/#{path}")
+  resp = RestClient.get("#{CONSUL}/#{path}")
+  return JSON.parse(Base64.decode64(JSON.parse(resp).pop['Value']))
 end
 
 def vault_get(path)
-  return RestClient.get(
+  resp = RestClient.get(
     "#{VAULT}/#{path}",
     headers = {'X-Vault-Token': VAULT_TOKEN}
   )
+  return JSON.parse(resp)['data']
 end
 
 def save_option(data)
-  File.write(OPTION_FILE, JSON.dump(data))
+  return File.write(OPTION_FILE, JSON.dump(data))
 end
 
-def save_env(team, project, env)
-  scm = Base64.decode64(JSON.parse(consul_get("#{team}/#{project}/_")).pop['Value'])
-  resp = JSON.parse(consul_get("#{team}/#{project}/#{env}")).pop
-
-  if resp.include?('Value')
-    File.open(ENV_FILE, 'w') { |file| file.truncate(0) }
-    data = JSON.parse(Base64.decode64(resp['Value']))
-
-    File.open(ENV_FILE, 'a') { |file|
-      file.puts "SCM=#{scm}"
-      file.puts "PAAS_TYPE=#{data['type']}"
-      file.puts "PAAS_APP=#{data['app']}"
-      file.puts "PAAS_ENVIRONMENT=#{data['environment']}"
+def save_env(file, data)
+  File.open(file, 'w') { |file| file.truncate(0) }
+  data.each { |key, value|
+    File.open(file, 'a') { |file|
+      file.puts "#{key}=#{value}"
     }
-
-    run = String.new
-    data['run'].each_with_index { |cmd, index|
-      if (index + 1) < data['run'].length
-        run += "#{cmd} && "
-      else
-        run += cmd
-      end
-    }
-    File.open(ENV_FILE, 'a') { |file|
-      file.puts "PAAS_RUN=\"#{run}\""
-    }
-
-    data['vars'].each { |var|
-      var.each { |k,v|
-        File.open(ENV_FILE, 'a') { |file|
-          file.puts "#{k}=#{v}"
-        }
-      }
-    }
-
-    if data['secrets']
-      secrets = JSON.parse(vault_get("#{team}/#{project}/#{env}"))['data']
-      secrets.each { |k,v|
-        unless k.eql?("") || v.eql?("")
-          File.open(ENV_FILE, 'a') { |file|
-            file.puts "#{k}=#{v}"
-          }
-        end
-      }
-    end
-  else
-    puts "Error: unable to parse Consul data."
-  end
-
+  }
 end
 
 def main(args)
   team, project, env = args
 
   if team.nil? || project.nil? || env.nil? || ENV['VAULT_TOKEN'].nil?
-    puts "Validating files: "
+    puts "Validating files:"
+
     config_files = Array.new
     Dir.foreach(CONFIG_DIR) do |file|
       if MIME::Types.type_for(file).to_s =~ /(text|application)\/(x-)?yaml/
@@ -104,22 +69,53 @@ def main(args)
     end
 
     puts "Updating Consul data."
+
     option_data = Hash.new
     config_files.each { |file|
       file_data = YAML.load_file(file)
-      consul_add("#{file_data['namespace']}/#{file_data['name']}/_", file_data['scm'])
-      env_data = []
+      env_data = Array.new
       file_data['environments'].each { |env|
         consul_add("#{file_data['namespace']}/#{file_data['name']}/#{env['environment']}", JSON.dump(env))
         env_data += [ env['environment'] ]
       }
+
+      consul_get("#{file_data['namespace']}/#{file_data['name']}/_")['environments'].each { |del|
+        consul_delete("#{file_data['namespace']}/#{file_data['name']}/#{del}") if not env_data.include?(del)
+      }
+
+      path_data = {
+        'name' => file_data['name'],
+        'namespace' => file_data['namespace'],
+        'scm' => file_data['scm'],
+        'environments' => env_data
+      }
+      consul_add("#{file_data['namespace']}/#{file_data['name']}/_", JSON.dump(path_data))
       option_data.deep_merge!({ file_data['namespace'] => { file_data['name'] => env_data }})
     }
-
     save_option(option_data)
+
   else
+
     puts "Saving environment variables."
-    save_env(team, project, env)
+
+    data = consul_get("#{team}/#{project}/#{env}")
+    run = String.new
+    data['run'].each_with_index { |cmd, index|
+      (index + 1) < data['run'].length ? run += "#{cmd} && " : run += cmd
+    }
+    file_content = {
+      'SCM' => consul_get("#{team}/#{project}/_")['scm'],
+      'PAAS_TYPE' => data['type'],
+      'PAAS_APP' => data['app'],
+      'PAAS_ENVIRONMENT' => data['environment'],
+      'PAAS_RUN' => "\"#{run}\""
+    }
+
+    data['vars'].each { |var| file_content.deep_merge!(var) }
+    secrets = vault_get("#{team}/#{project}/#{env}") if data['secrets']
+    file_content.deep_merge!(secrets)
+
+    save_env(ENV_FILE, file_content)
   end
 
 end
