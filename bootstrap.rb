2852499acc26156b3301bd71e595b98c4f1f9b50
Bowen Sun <bowensun@gmail.com>
2018-03-27 18:16:09 +0100

    fix PAAS_RUN envar

--- bootstrap.rb
+++ bootstrap.rb
@@ -114,7 +114,7 @@ def main(args)
       'PAAS_TYPE' => data['type'],
       'PAAS_APP' => data['app'],
       'PAAS_ENVIRONMENT' => data['environment'],
-      'PAAS_RUN' => data['run'].empty? ? "''" : run
+      'PAAS_RUN' => data['run'].empty? ? nil : run
     }
     data['vars'].each { |var| file_content.deep_merge!(var) } unless data['vars'].empty?
     if data['secrets']
